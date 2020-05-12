4663d77ad60b4f4c64b87adfbeccab5638d24ae7
Zachary Zensen <TechnomagezZ@yahoo.com>
2020-05-11 20:27:33 -0700

    have a generic idea but could not finish before due date

--- src/exercises/_1_WordFinder.java
+++ src/exercises/_1_WordFinder.java
@@ -6,13 +6,24 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class _1_WordFinder {
-
+public class _1_WordFinder{
+	
+	
 	public static void main(String[] args) {
 		/* Print all the words beginning with P that are over 10 characters. */
+		WordsList wordsList = new WordsList(false, false);
 		
-
+		List<wordsList> wordList = new List<wordsList>(loadWordList());
+		
+		Stream<wordsList> Words = wordList.stream();
+				
+		List<wordsList> wordsWithP = Words.filter(wordsList -> wordsList.hasP().overTen()
+				.collect(Collectors.toList()));
+				
+		System.out.println(wordsWithP);
 		
 	}
 
@@ -32,5 +43,7 @@ public class _1_WordFinder {
 
 		return messages;
 	}
+	
+	
 
 }
