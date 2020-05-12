bfa8c6073c8a8b94acc54ef1a7bc67968cb4beba
Zachary Zensen <TechnomagezZ@yahoo.com>
2020-05-11 20:32:38 -0700

    Got it partially but ran out of time before due date.

--- src/exercises/_3_JobCandidateSelectorUsingLambdas.java
+++ src/exercises/_3_JobCandidateSelectorUsingLambdas.java
@@ -1,10 +1,16 @@
 package exercises;
 
-public class _3_JobCandidateSelectorUsingLambdas {
-	public static void main(String[] args) {
+import java.util.Collections;
+import java.util.List;
 
+public class _3_JobCandidateSelectorUsingLambdas{
+	public static void main(String[] args) {
+		JobCandidate jobCandidate = new JobCandidate();
+		List<JobCandidate> candidates= JobCandidate.jobCandidates;
+		int salary = jobCandidate.getSalaryRequired();
 		// 1. Sort the candidates by salary requirements, low to high.
 		
+		Collections.sort(candidates, );
 
 		// 2. Filter out any candidates that have not been taught by me (June)
 		// Hint: see the removeIf() method in the Collections class
